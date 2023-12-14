ff4ebbbe8b4f94d957bd059439322dacd8463c33
zhougang <921366807@qq.com>
2023-12-14 20:42:46 +0800

    feat: 更新

--- one-api-backend/src/main/java/top/charjin/oneapi/backend/config/Knife4jConfig.java
+++ one-api-backend/src/main/java/top/charjin/oneapi/backend/config/Knife4jConfig.java
@@ -1,36 +1,36 @@
 package top.charjin.oneapi.backend.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.HeaderParameter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Knife4j 接口文档配置
  * https://doc.xiaominfo.com/knife4j/documentation/get_start.html
  */
 @Configuration
-@EnableSwagger2
 @Profile({"dev", "test"})
 public class Knife4jConfig {
 
     @Bean
-    public Docket defaultApi2() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(new ApiInfoBuilder()
-                        .title("接口文档")
-                        .description("oneapi")
-                        .version("1.0")
-                        .build())
-                .select()
-                // 指定 Controller 扫描包路径
-                .apis(RequestHandlerSelectors.basePackage("top.charjin.oneapi.backend.controller"))
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addParameters("token", new HeaderParameter().description("请填写Token").schema(new StringSchema()))
+                        .addParameters("adminID", new HeaderParameter().description("请填写用户ID").schema(new StringSchema())))
+                .info(new Info().title("oneapi")
+                        .description("Knife4j增强文档")
+                        .version("v2.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("二饭快速开发框架")
+                        .url("https://gitee.com/StandFast"));
     }
 }
